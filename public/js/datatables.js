e76756ac2f9a90f8773466dd14f8b58cda558d04
Michelium <michelhamelink@gmail.com>
2020-04-04 11:12:08 +0200

    private_html to public rename config changes

--- public/js/datatables.js
+++ public/js/datatables.js
@@ -5568,7 +5568,7 @@
 	
 		/* If the number of columns in the DOM equals the number that we have to
 		 * process in DataTables, then we can use the offsets that are created by
-		 * the private_html- browser. No custom sizes can be set in order for this to happen,
+		 * the public- browser. No custom sizes can be set in order for this to happen,
 		 * nor scrolling used
 		 */
 		if ( ie67 || ! userInputs && ! scrollX && ! scrollY &&
@@ -13097,7 +13097,7 @@
 			"bCollapse": null,
 	
 			/**
-			 * Width of the scrollbar for the private_html-browser's platform. Calculated
+			 * Width of the scrollbar for the public-browser's platform. Calculated
 			 * during table initialisation.
 			 *  @type int
 			 *  @mainpages 0
